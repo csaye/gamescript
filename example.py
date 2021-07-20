cab1fecfbef4d4a8540b267c7ee58238b0ef2e93
Cooper Saye <coopersaye@gmail.com>
2021-07-19 21:30:10 -0700

    Update example.

--- example.py
+++ example.py
@@ -4,7 +4,15 @@ import gamescript
 game = gamescript.Game()
 game.caption('example project')
 
+# draws frame and text to screen
+def draw():
+    game.draw() # draw board
+    game.write(frame, gamescript.white, 0, 0) # write frame
+    game.display() # display changes
+
 # game loop
+frame = 0
+draw()
 while True:
     # tick game loop
     game.tick() # tick game
@@ -16,7 +24,9 @@ while True:
     if game.ismousedown('left'): # if left mouse button pressed
         x, y = game.mousepos() # get mouse position
         game.board[x][y] = gamescript.white # set board to white
+        draw() # draw text and board
 
     # tick update loop
     if game.isupdate():
-        pass
+        frame += 1 # increment frame
+        draw() # draw text and board
